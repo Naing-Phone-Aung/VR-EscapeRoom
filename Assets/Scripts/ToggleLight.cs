adad5989e6ba8635b4059913b5346f89f200a786
MinPyaeHmoo <minpyaehmoo@gmail.com>
2025-05-18 03:02:12 +0800

    MPH_V1.1
    3rd update from MPH

--- Assets/Scripts/ToggleLight.cs
+++ Assets/Scripts/ToggleLight.cs
@@ -1,19 +1,44 @@
 using UnityEngine;
+using TMPro;
 
 public class ToggleLight : MonoBehaviour
 {
-    public Light currentLight = null;   // reference to light source 
-    public bool isOn = false;    // light source?s state
+    public Light currentLight = null;
+    public bool isOn = false;
+    public Collider targetCollider = null;
 
+    // Optional: reference to the text to force-hide
+    public TextMeshPro revealText;
 
     void Start()
     {
-        currentLight.enabled = isOn;    // initialise light to on/off 
-    }
-    // toggles the light on/off 
-    public void ToggleLightOnOff()
+        currentLight.enabled = isOn;
+        if (targetCollider != null)
+            targetCollider.enabled = isOn;
 
+        if (revealText != null)
+            SetAlpha(revealText, isOn ? 1f : 0f);
+    }
+
+    public void ToggleLightOnOff()
     {
-        currentLight.enabled = !currentLight.enabled;
+        isOn = !isOn;
+        currentLight.enabled = isOn;
+
+        if (targetCollider != null)
+            targetCollider.enabled = isOn;
+
+        if (!isOn && revealText != null)
+        {
+            // force hide text when light off
+            SetAlpha(revealText, 0f);
+        }
+    }
+
+    private void SetAlpha(TextMeshPro tmp, float alpha)
+    {
+        Color c = tmp.faceColor;
+        c.a = alpha;
+        tmp.faceColor = c;
     }
 }
