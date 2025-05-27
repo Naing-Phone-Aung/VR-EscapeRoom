3bfd91ea4c1a35f5f16374e1b3d590ff9774d02d
Pyae Sone Aung <pyaesone.mdy95@gmail.com>
2025-05-27 16:28:37 +0800

    update

---
+++ Assets/Scripts/ForestEscape/SliceObject3.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using EzySlice;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+
+public class SliceObject : MonoBehaviour
+{
+    public Transform startSlicePoint;
+    public Transform endSlicePoint;
+    public VelocityEstimator velocityEstimator;
+    public LayerMask sliceableLayer;
+    public Material crossSectionMaterial;
+    public float cutForce = 2000;
+
+    void FixedUpdate()
+    {
+        bool hasHit = Physics.Linecast(startSlicePoint.position, endSlicePoint.position, out RaycastHit hit, sliceableLayer);
+        if (hasHit) 
+        {
+            GameObject target = hit.transform.gameObject;
+            Slice(target);
+        }
+    }
+
+    public void Slice(GameObject target)
+    {
+        Vector3 velocity = velocityEstimator.GetVelocityEstimate();
+        Vector3 planeNormal = Vector3.Cross(endSlicePoint.position - startSlicePoint.position, velocity);
+        planeNormal.Normalize();
+
+        SlicedHull hull = target.Slice(endSlicePoint.position, planeNormal);
+
+        if (hull != null)
+        {
+            GameObject upperHull = hull.CreateUpperHull(target, crossSectionMaterial);
+            SetupSlicedComponenet(upperHull);
+
+            GameObject lowerHull = hull.CreateLowerHull(target, crossSectionMaterial);
+            SetupSlicedComponenet(lowerHull);
+
+            Destroy(target);
+        }
+    }
+
+    public void SetupSlicedComponenet(GameObject slicedObject)
+    {
+        Rigidbody rb = slicedObject.AddComponent<Rigidbody>();
+        MeshCollider collider = slicedObject.AddComponent<MeshCollider>();
+        collider.convex = true;
+        rb.AddExplosionForce(cutForce, slicedObject.transform.position,1);
+    }
+}
